15d84da8d1139c9b1d9c470e5f75b96ceac33af7
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-22 13:50:34 -0700

    New Work Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove ();
+/* const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+document.body.append(newHeader);
+newHeader.innerHTML = "<h1 id='victory'>YOUR-NAME is the champion</h1>"; */
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "JavaScript is the champion";
